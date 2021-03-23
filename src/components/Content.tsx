a2fcf55ffeefb4760ad9e430b9199f5283be57b6
santosant <hiroto.anderson@gmail.com>
2021-03-22 23:56:01 -0300

    Prop drilling w/ React Context API

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
+import { MovieCard } from '../components/MovieCard';
+import { useMovieContext } from '../hooks/MoviesContext';
+import '../styles/content.scss';
+import '../styles/global.scss';
+
 export function Content() {
-  // Complete aqui
-}
+    const { movies, selectedGenre } = useMovieContext();
+
+    return (
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    )
+  }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
+import { Button } from './Button';
+import '../styles/global.scss';
+import '../styles/sidebar.scss';
+import { useMovieContext } from '../hooks/MoviesContext';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, handleClickButton, selectedGenreId } = useMovieContext();
+
+  return (
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  )
 }
